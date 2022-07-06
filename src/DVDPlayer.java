e5e53b2e940ee1a6f26d3aea38f786c43d236625
pockocmoc <pockocmoc@inbox.ru>
2022-07-06 17:50:19 +0300

    Initial commit

---
+++ src/DVDPlayer.java
@@ -0,0 +1,12 @@
+public class DVDPlayer {
+    boolean canRecord = false;
+
+    void recordDVD() {
+        System.out.println("идёт запись DVD");
+
+
+    }
+    void playDVD() {
+        System.out.println("воспроизводится DVD");
+    }
+}
